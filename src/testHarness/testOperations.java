1c9b9b42fd4b1e4336f481c4ffa56f321c49b875
cvidan <cvidan@uwo.ca>
2018-04-08 22:03:18 -0400

    added list population method

--- src/testHarness/testOperations.java
+++ src/testHarness/testOperations.java
@@ -12,6 +12,7 @@ import customDatatypes.NotificationTypes;
 import offerings.CourseOffering;
 import offerings.ICourseOffering;
 import offerings.OfferingFactory;
+import registrar.ModelRegister;
 import systemUsers.AdminModel;
 import systemUsers.InstructorModel;
 import systemUsers.StudentModel;
@@ -35,6 +36,8 @@ public class testOperations {
 		InstructorModel instructor = null;
 		Boolean online = false;
 		
+		// populate those lists
+		populateLists();		
 	
 		AdminModel defaultAdmin = new AdminModel();
 		defaultAdmin.setName("default");
@@ -729,6 +732,56 @@ public class testOperations {
 			return null;
 		}else {
 			return null;
+		}
+	}
+	
+	private static void populateLists() throws IOException {
+		try {
+			// Create an instance of an OfferingFactory
+			OfferingFactory factory = new OfferingFactory();
+			BufferedReader br = new BufferedReader(new FileReader(new File("note_1.txt")));
+			// Use the factory to populate as many instances of courses as many files we've got.
+			CourseOffering	courseOffering = factory.createCourseOffering(br);
+			br.close();
+			// Loading 1 file at a time
+			br = new BufferedReader(new FileReader(new File("note_2.txt")));
+			// here we have only two files
+			courseOffering = factory.createCourseOffering(br);
+			br.close();
+			// code to perform sanity checking of all our code
+			// by printing all of the data that we've loaded
+			for(CourseOffering course : ModelRegister.getInstance().getAllCourses()){
+//				System.out.println("ID : " + course.getCourseID() + "\nCourse name : " + course.getCourseName() + "\nSemester : " + 
+//				course.getSemester());
+//				System.out.println("Students allowed to enroll\n");
+				courseList.add(course);
+				
+				for(StudentModel student : course.getStudentsAllowedToEnroll()){
+//					System.out.println("Student name : " + student.getName() + "\nStudent surname : " + student.getSurname() + 
+//							"\nStudent ID : " + student.getID() + "\nStudent EvaluationType : " + 
+//							student.getEvaluationEntities().get(course) + "\n\n");
+					studentList.add(student);
+					if (student.getCoursesAllowed() == null)
+						student.setCoursesAllowed(new ArrayList<ICourseOffering>());
+					student.getCoursesAllowed().add(course);
+					
+				}
+				
+				for (InstructorModel instructor : course.getInstructor()) {
+					instructorList.add(instructor);
+					if (instructor.getIsTutorOf() == null)
+						instructor.setIsTutorOf(new ArrayList<ICourseOffering>());
+					instructor.getIsTutorOf().add(course);
+				}
+				
+//				for(StudentModel student : course.getStudentsAllowedToEnroll()){
+//					for(ICourseOffering course2 : student.getCoursesAllowed())
+//					System.out.println(student.getName() + "\t\t -> " + course2.getCourseName());
+//				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Files for populating the lists were not found (names note_1.txt and note_2.txt)");
+			System.exit(1);
 		}
 	}
 		
